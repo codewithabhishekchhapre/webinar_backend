0a3af93077bfee1093e63c71195cfec346261110
Devpot-abhi <abhishekgurjar5020@gmail.com>
2025-10-08 14:10:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
      try {
-          await mongoose.connect("mongodb://localhost:27017/mywebinardatabase");
+          await mongoose.connect("mongodb+srv://devpot076_db_user:YIrtY0KIkVV89TP8@cluster0.yajfcg3.mongodb.net/baserahub_webinar?retryWrites=true&w=majority&appName=Cluster0");
           console.log("✅ MongoDB connected successfully!");
      }
      catch (err) {
